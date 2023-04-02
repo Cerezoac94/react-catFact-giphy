1d9b37602ffe43f922dbaca31f0eb6e51100e93e
Jorge Cerezo <antonio.cerezo94@hotmail.com>
2023-04-01 22:36:17 -0600

    fix(.env): Fix file .env and add env example

--- src/hooks/useGiphy.jsx
+++ src/hooks/useGiphy.jsx
@@ -1,16 +1,12 @@
 import { useState, useEffect } from "react";
 import { GIPHY_URL } from "../config/constants";
 
-// const API_KEY_GIPHY = import.meta.env.API_KEY_GIPHY
-// console.log(API_KEY_GIPHY);
-const API_KEY_GIPHY = "GCcfYLkxbuR1pclXCE1MQ8IzsuJmhMBX";
-
 export const useGiphy = (word) => {
+	const API_KEY_GIPHY = import.meta.env.VITE_API_KEY_GIPHY;
+	const SUFFIX_URL = `?api_key=${API_KEY_GIPHY}&q=${word}&limit=20`;
 	const [gif, setGif] = useState();
 	const fetchGif = async () => {
-		const results = await fetch(
-			`${GIPHY_URL}?api_key=${API_KEY_GIPHY}&q=${word}&limit=20`
-		);
+		const results = await fetch(`${GIPHY_URL}${SUFFIX_URL}`);
 		const { data } = await results.json();
 		const url = await data[0].images.original.url;
 		setGif(url);
